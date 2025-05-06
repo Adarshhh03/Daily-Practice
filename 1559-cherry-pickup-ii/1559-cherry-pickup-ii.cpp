6e4214649910cd02f07c9532aa08d145c8f39d94
Adarshhh03 <106335157+Adarshhh03@users.noreply.github.com>
2025-05-06 23:40:49 +0530

    Time: 51 ms (53.78%), Space: 20.8 MB (80.61%) - LeetHub

---
+++ 1559-cherry-pickup-ii/1559-cherry-pickup-ii.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int solve(int i,int j1,int j2,vector<vector<int>>& grid,int n,int m,vector<vector<vector<int>>> &dp){
+    if(j1<0 || j2<0 || j1>m-1 || j2>m-1){
+        return -1e9;
+    }
+
+    if(i==n-1){
+        if(j1==j2){
+            return grid[i][j1];
+        }
+        else{
+            return grid[i][j1]+grid[i][j2];
+        }
+    }
+    if(dp[i][j1][j2] != -1) return dp[i][j1][j2];
+
+    int maxi=INT_MIN;
+    for(int di=-1;di<=1;di++){
+    for(int dj=-1;dj<=1;dj++){
+        int val;
+        if(j1==j2){
+            val=grid[i][j1]+solve(i+1,j1+di,j2+dj,grid,n,m,dp);
+        }
+        else{
+            val=grid[i][j1]+grid[i][j2]+solve(i+1,j1+di,j2+dj,grid,n,m,dp);
+        }
+        maxi=max(maxi,val);
+    }
+    }
+    return dp[i][j1][j2]=maxi;
+    }
+
+    int cherryPickup(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+
+        vector<vector<vector<int>>> dp(n,vector<vector<int>> (m,vector<int> (m,-1)));
+        
+        return solve(0,0,m-1,grid,n,m,dp);
+          
+    }
+};
