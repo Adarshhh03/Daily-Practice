23ae1b9f55dc42fdc69ea5c5b6e99a14c0b4da0c
Adarshhh03 <106335157+Adarshhh03@users.noreply.github.com>
2025-05-10 18:32:00 +0530

    Time: 27 ms (55.37%), Space: 27.6 MB (27.64%) - LeetHub

---
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    // int solve(int index1,int index2,string text1, string text2){
+    //     if(index1<0 || index2<0){
+    //         return 0;
+    //     }
+    //     int op1=0,op2=0;
+    //     if(text1[index1]==text2[index2]){
+    //   op1=1+solve(index1-1,index2-1,text1,text2);
+    //     }
+    //     else{
+    //     op2=max(solve(index1-1,index2,text1,text2),solve(index1,index2-1,text1,text2));
+    //     }
+
+    //     return  max(op1,op2);
+    // }
+    int longestCommonSubsequence(string text1, string text2) {
+        int index1=text1.length();
+        int index2=text2.length();
+        vector<vector<int>> dp(index1+1,vector<int> (index2+1,0));
+        for(int i=1;i<=index1;i++){
+            for(int j=1;j<=index2;j++){
+                int op1=0,op2=0;
+                if(text1[i-1]==text2[j-1]){
+                 op1=1+dp[i-1][j-1];
+                }
+                else{
+                    op2=max(dp[i-1][j],dp[i][j-1]);
+                }
+
+                dp[i][j]=max(op1,op2);
+            }
+        }
+
+        return dp[index1][index2];
+    }
+};
