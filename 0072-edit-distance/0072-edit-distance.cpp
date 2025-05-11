d08d4e4391ee2492c5fef38ea4d5f927e42a3e34
Adarshhh03 <106335157+Adarshhh03@users.noreply.github.com>
2025-05-11 13:38:31 +0530

    Time: 10 ms (36.84%), Space: 13.1 MB (61.25%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -2,27 +2,50 @@
 
 class Solution {
 public:
-    int solve(int i, int j, string &s1, string &s2, vector<vector<int>> &dp) {
-        if (i < 0) return j + 1;  // Need to insert all remaining chars of s2
-        if (j < 0) return i + 1;  // Need to delete all remaining chars of s1
+    // int solve(int i, int j, string &s1, string &s2, vector<vector<int>> &dp) {
+    //     if (i < 0) return j + 1;  // Need to insert all remaining chars of s2
+    //     if (j < 0) return i + 1;  // Need to delete all remaining chars of s1
 
-        if (dp[i][j] != -1) return dp[i][j];
+    //     if (dp[i][j] != -1) return dp[i][j];
 
-        if (s1[i] == s2[j]) {
-            return dp[i][j] = solve(i - 1, j - 1, s1, s2, dp);
-        } else {
-            return dp[i][j] = 1 + min({
-                solve(i, j - 1, s1, s2, dp),    // Insert
-                solve(i - 1, j, s1, s2, dp),    // Delete
-                solve(i - 1, j - 1, s1, s2, dp) // Replace
-            });
-        }
-    }
+    //     if (s1[i] == s2[j]) {
+    //         return dp[i][j] = solve(i - 1, j - 1, s1, s2, dp);
+    //     } else {
+    //         return dp[i][j] = 1 + min({
+    //             solve(i, j - 1, s1, s2, dp),    // Insert
+    //             solve(i - 1, j, s1, s2, dp),    // Delete
+    //             solve(i - 1, j - 1, s1, s2, dp) // Replace
+    //         });
+    //     }
+    // }
 
     int minDistance(string word1, string word2) {
         int n = word1.size();
         int m = word2.size();
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        return solve(n - 1, m - 1, word1, word2, dp);
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+    //when word1 is zero,so equal insertions
+    for(int i=1;i<=n;i++){
+        dp[i][0]=i;
+    }
+    //when word2 is zero,so equal deletions
+     for(int i=1;i<=m;i++){
+        dp[0][i]=i;
+    }
+
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            int op=0;
+            if(word1[i-1]==word2[j-1]){
+                op=0+dp[i-1][j-1];
+            }
+            else{
+                op=1+min({dp[i][j-1],dp[i-1][j],dp[i-1][j-1]});
+            }
+
+            dp[i][j]=op;
+        }
+    }
+        return dp[n][m];
+        
     }
 };
