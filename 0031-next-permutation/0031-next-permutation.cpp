02f9950bb2e1718ff795a69e0cf3c91482bc9cc9
Adarshhh03 <106335157+Adarshhh03@users.noreply.github.com>
2025-05-25 20:04:01 +0530

    Time: 0 ms (100%), Space: 15.7 MB (32.75%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+//Intuition 
+// Step 1)FInd the first smaller number from the back,i.e breakng point which has chances of giving next permutation
+// Step 2)Now find first next greater element than it from the right and swap them.
+// Step 3)Reverse the part next to the breaking point as it the biggest part now for the new left side after swapping
    vector<int> nextPermutation (vector<int> &A) {
               int ind = -1;
          int n = A.size();
