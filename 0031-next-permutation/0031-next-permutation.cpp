53afb077f7420efd6003c522f8c000072852e9af
Adarshhh03 <106335157+Adarshhh03@users.noreply.github.com>
2025-06-17 22:48:46 +0530

    Time: 0 ms (100%), Space: 15.6 MB (92.49%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,29 +1,32 @@
 class Solution {
 public:
-//Intuition 
-// Step 1)FInd the first smaller number from the back,i.e breakng point which has chances of giving next permutation
-// Step 2)Now find first next greater element than it from the right and swap them.
-// Step 3)Reverse the part next to the breaking point as it the biggest part now for the new left side after swapping
-   vector<int> nextPermutation (vector<int> &A) {
-              int ind = -1;
-         int n = A.size();
-         for(int i = n - 2;i>=0; i--) { 
-            if(A[i] < A[i+1]) {
-            ind = i; 
-            break;
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        int index = -1;
+
+        // Step 1: Find first decreasing element from the end
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                index = i;
+                break;
             }
-   }
-    if(ind == -1){
-         reverse(A.begin(), A.end()); 
-         return A;
-   }
-       for(int i = n-1;i>ind; i--) { 
-              if(A[i]>A[ind]) { 
-                     swap(A[i], A[ind]); 
-                     break;
-              }
         }
-    reverse(A.begin()+ind+ 1, A.end()); 
-    return A;
+
+        // Step 2: If no such element, reverse whole array
+        if (index == -1) {
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+
+        // Step 3: Find the next greater element on right side
+        for (int i = n - 1; i > index; i--) {
+            if (nums[i] > nums[index]) {
+                swap(nums[i], nums[index]);
+                break;
+            }
+        }
+
+        // Step 4: Reverse the part after the index
+        reverse(nums.begin() + index + 1, nums.end());
     }
 };
