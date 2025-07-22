e0e6e158d498a2e50278cee82e2aafc6ae97efcb
Adarshhh03 <106335157+Adarshhh03@users.noreply.github.com>
2025-07-22 22:37:32 +0530

    Time: 0 ms (100%), Space: 14.9 MB (59.52%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -5,18 +5,15 @@ public:
         ListNode* fast = dummy;
         ListNode* slow = dummy;
 
-        // Move fast n+1 steps ahead to maintain a gap of n
         for (int i = 0; i <= n; i++) {
             fast = fast->next;
         }
 
-        // Move both fast and slow together until fast reaches end
         while (fast != NULL) {
             fast = fast->next;
             slow = slow->next;
         }
 
-        // Remove the nth node from end
         ListNode* toDelete = slow->next;
         slow->next = slow->next->next;
         delete toDelete;
