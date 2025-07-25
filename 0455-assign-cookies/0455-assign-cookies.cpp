895dc9df00d94b75d14e1a4a693c71a10fc61176
Adarshhh03 <106335157+Adarshhh03@users.noreply.github.com>
2025-07-25 08:45:14 +0530

    Time: 8 ms (62.12%), Space: 44.8 MB (43.78%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
-    int solve(vector<int>& g, vector<int>& s,int n,int m,int ind1,int ind2){
-        if(ind1>=n || ind2>=m)  return 0;
-        int take=0,notTake=0;
-        if(g[ind1]<=s[ind2]){
-            take=1+solve(g,s,n,m,ind1+1,ind2+1);
-        }
-        else{
-            notTake=0+solve(g,s,n,m,ind1,ind2+1);
-        }
-
-        return max(take,notTake);
-    }
     int findContentChildren(vector<int>& g, vector<int>& s) {
-        int n=g.size();
-        int m=s.size();
-        sort(g.begin(),g.end());
-        sort(s.begin(),s.end());
+        sort(g.begin(), g.end()); 
+        sort(s.begin(), s.end()); 
 
-        return solve(g,s,n,m,0,0);
+        int i = 0, j = 0;
+        int cnt = 0;
+
+        while(i < g.size() && j < s.size()) {
+            if(s[j] >= g[i]) {
+             
+                cnt++;
+                i++;
+                j++;
+            } else {
+               
+                j++;
+            }
+        }
+
+        return cnt;
     }
 };
