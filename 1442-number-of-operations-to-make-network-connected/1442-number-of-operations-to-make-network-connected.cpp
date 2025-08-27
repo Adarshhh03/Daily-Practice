b039b6b1ccff72c7c67fbcc7733fd0ec08b42644
Adarshhh03 <106335157+Adarshhh03@users.noreply.github.com>
2025-08-28 00:18:49 +0530

    Time: 7 ms (75.44%), Space: 42.5 MB (75.37%) - LeetHub

---
+++ 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class DisjointSet {
+    vector<int> rank, parent, size;
+public:
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]); // path compression
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u;
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+};
+
+class Solution {
+public:
+    int makeConnected(int n, vector<vector<int>>& connections) {
+   
+        if (connections.size() < n - 1) return -1;
+
+        DisjointSet ds(n);
+
+        
+        for (auto &edge : connections) {
+            int u = edge[0];
+            int v = edge[1];
+            ds.unionByRank(u, v);
+        }
+
+ 
+        int components = 0;
+        for (int i = 0; i < n; i++) {
+            if (ds.findUPar(i) == i) components++;
+        }
+
+        return components - 1;
+    }
+};
